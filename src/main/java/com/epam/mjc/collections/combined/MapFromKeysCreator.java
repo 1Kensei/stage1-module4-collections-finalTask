02c6aa66de267eaf72e8b10598a494ce59dfe1f6
diasbuz <grandleocraft@gmail.com>
2025-07-27 18:41:29 +0600

    Commit

--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -1,9 +1,18 @@
 package com.epam.mjc.collections.combined;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class MapFromKeysCreator {
     public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
+        Map<Integer, Set<String>> map = new HashMap<>();
+        for (String line : sourceMap.keySet()) {
+            Set<String> set = map.getOrDefault(line.length(), new HashSet<>());
+            set.add(line);
+            map.put(line.length(), set);
+        }
+        return map;
     }
 }
